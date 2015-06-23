ca2b00f4ade84a0c2a9409d7fbe0bed6472cc865
Justin Ognoskie <oggie7797@gmail.com>
2015-06-23 13:26:45 -0500

    Axxionize v2

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2013 Cybojenix <anthonydking@gmail.com>
-# Copyright (C) 2013 The OmniROM Project
+# The Axxion Project
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
